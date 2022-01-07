245b80f17da0c266ef2b7396791cd226e9164759
3v <87390765+3visual@users.noreply.github.com>
2022-01-06 23:57:48 -0600

    Create main.js

---
+++ _assets/js/main.js
@@ -0,0 +1,23 @@
+function gS() {
+	$.getJSON("https://api.lanyard.rest/v1/users/730087630662008873", data => {
+		data = data.data;
+		if (data.listening_to_spotify) {
+			$("#spotify").html(
+				`<a class="noAStyle" target="_blank" href="https://open.spotify.com/track/${data.spotify.track_id}"><b>${data.spotify.song}</b> by <i>${data.spotify.artist}</i></a> on <b><a target="_blank" class="noAStyle" href="https://spotify.gg/">Spotify</a></b>`
+			);
+		} else {
+			$("#spotify").html("Not listening to anything");
+		}
+		if (data.active_on_discord_mobile && !data.active_on_discord_desktop) {
+			$("#status").css("color", "#F2A6AB");
+		} else if (data.active_on_discord_desktop) {
+			$("#status").css("color", "#abffbf");
+		} else {
+			$("#status").css("color", "#d4d4d4");
+		}
+	});
+}
+
+gS();
+
+setInterval(gS, 5000);
